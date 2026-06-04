95181b8446a10bbf9a87fc54098e7ff8a85add13
abhayvermavansh-tech <abhayvermavansh@gmail.com>
2026-06-04 23:56:04 +0530

    Update water_tank.py

--- classwork/iteration_4th_june/water_tank.py
+++ classwork/iteration_4th_june/water_tank.py
@@ -1,11 +1,13 @@
 #program for different water levels:
+#initialize constants:
 water_level=0
-
+#---------------------------------------------
 #iteration begins:
 while(water_level<100):
     #increase in water level:
     water_level = water_level+10
     print("Water Level:",water_level,"liters")
-
+#_--------------------------------------------    
 #when tank is full:
 print("Tank is full.")
+#---------------------------------------------
