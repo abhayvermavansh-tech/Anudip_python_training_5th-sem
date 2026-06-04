37b8ad4351d67c0b445b03c7a96da120b89cf5ab
abhayvermavansh-tech <abhayvermavansh@gmail.com>
2026-06-05 00:17:16 +0530

    Create attendence_counter.py

---
+++ classwork/iteration_4th_june/attendence_counter.py
@@ -0,0 +1,19 @@
+#program for attendence counter:
+#initialize the constants:
+student = 1
+attendence = 0
+#-------------------------------------------
+#iteration begins:
+while(student <=30):
+    if(student):
+        #attendence increases:
+        print("Student Entered")
+        attendence = attendence +1
+        #-----------------------------------
+        print("Attendence Count:",attendence,"\n" )
+    else:
+        print("zero student")
+    student = student +1 
+#-------------------------------------------    
+print("All students are present.") 
+#-------------------------------------------
