521941443a43660ee25795dadf699406151284d0
abhayvermavansh-tech <abhayvermavansh@gmail.com>
2026-06-04 23:57:47 +0530

    Update battery_level.py

--- classwork/iteration_4th_june/battery_level.py
+++ classwork/iteration_4th_june/battery_level.py
@@ -1,9 +1,14 @@
 #Program for battery level:
+#initialize constants:
 charging = 20
+#----------------------------------------
 #iteration begins:
 while(charging<100):
     #Increase in battery level
     charging = charging+10
+    #------------------------------------
     print("Battery_Level:",charging,"%")
+#----------------------------------------    
 #When battery is full:    
 print("Battery Fully Charged.")
+#----------------------------------------
