c987e0e6838fd2b05c844eea05a980ea7fd7e5b4
abhayvermavansh-tech <abhayvermavansh@gmail.com>
2026-06-04 23:44:35 +0530

    Create Battery_level.py

---
+++ classwork/iteration_4th_june/Battery_level.py
@@ -0,0 +1,9 @@
+#Program for battery level:
+charging = 20
+#iteration begins:
+while(charging<100):
+    #Increase in battery level
+    charging = charging+10
+    print("Battery_Level:",charging,"%")
+#When battery is full:    
+print("Battery Fully Charged.")
