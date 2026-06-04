c571ee6f1d9b06907aa434c4c9f6b55f478bddf9
abhayvermavansh-tech <abhayvermavansh@gmail.com>
2026-06-04 23:53:43 +0530

    Create attendence_tracker.py

---
+++ classwork/iteration_4th_june/attendence_tracker.py
@@ -0,0 +1,20 @@
+#program for attendence tracker:
+#initialize the constants:
+student =1
+present =0
+absent =0
+#--------------------------------------
+#Iteration begins:
+while (student <=5):
+    print("student",student,":")
+    attendence = input("attendence:")
+    #If the student is present or not:
+    if(attendence == "present"):
+        present = present+1
+    else:
+        absent = absent +1
+    student=student+1    
+#--------------------------------------    
+print("no. of student present =",present)
+print("no. of student absent =",absent) 
+#--------------------------------------
