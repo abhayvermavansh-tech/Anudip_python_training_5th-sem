76deb2d098857b0b18d5a72048a488e6e0e04007
abhayvermavansh-tech <abhayvermavansh@gmail.com>
2026-06-04 23:37:14 +0530

    Update and rename classwork/area_perimeter(triangle).py to classwork/iteration_4th_june/water_tank.py

---
+++ classwork/iteration_4th_june/water_tank.py
@@ -0,0 +1,11 @@
+#program for different water levels:
+water_level=0
+
+#iteration begins:
+while(water_level<100):
+    #increase in water level:
+    water_level = water_level+10
+    print("Water Level:",water_level,"liters")
+
+#when tank is full:
+print("Tank is full.")
